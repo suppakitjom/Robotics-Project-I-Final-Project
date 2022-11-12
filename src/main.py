0ed9b5bb36724c3a46c866c140eadbd504fc957e
suppakitjom <99940355+suppakitjom@users.noreply.github.com>
2022-11-12 10:54:22 +0700

    refined code

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 """
-main file for the spotify remote
+main file for the Spotify remote
 uses pySerial to communicate with ESP32 board
 """
 
@@ -7,33 +7,45 @@ import serial
 import time
 from spotipyHelper import *
 
-ser = serial.Serial('/dev/cu.usbserial-53220232121', 115200, timeout=1)
-time.sleep(1)
+print('\033[92m' + "Starting up..." + '\033[0m')
+ser = serial.Serial()
 
-while True:
-    text = ser.readline().decode('utf-8').strip()
-    if text:
-        print(text)
-    else:
-        print('No data, retrying...')
+try:
+    ser.port = '/dev/cu.usbserial-53220232121'
+    ser.baudrate = 115200
+    ser.timeout = 1
+    ser.open()
+    while True:
+        text = ser.readline().decode('utf-8').strip()
+        if text:
+            print(text)
+        else:
+            print('No data, retrying...')
 
-    if text == 'Toggle Playback':
-        togglePlayBack()
-    elif text == 'Toggle Repeat':
-        toggleRepeat()
-    elif text == 'Toggle Shuffle':
-        toggleShuffle()
-    elif text == 'Next':
-        nextTrack()
-    elif text == 'Previous':
-        previousTrack()
+        if text == 'Toggle Playback':
+            togglePlayBack()
+        elif text == 'Toggle Repeat':
+            toggleRepeat()
+        elif text == 'Toggle Shuffle':
+            toggleShuffle()
+        elif text == 'Next':
+            nextTrack()
+        elif text == 'Previous':
+            previousTrack()
 
-    if text == 'ALBUM CHARLIE':
-        # https://open.spotify.com/album/2LTqBgZUH4EkDcj8hdkNjK
-        playAlbum('spotify:album:2LTqBgZUH4EkDcj8hdkNjK')
-    elif text == 'ALBUM CERTIFIED LOVER BOY':
-        # https://open.spotify.com/album/3SpBlxme9WbeQdI9kx7KAV
-        playAlbum('spotify:album:3SpBlxme9WbeQdI9kx7KAV')
-    elif text == 'TRACK NEVER GONNA GIVE YOU UP':
-        # https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT
-        playTrack('spotify:track:4cOdK2wGLETKBW3PvgPWqT')
+        if text == 'ALBUM CHARLIE':
+            # https://open.spotify.com/album/2LTqBgZUH4EkDcj8hdkNjK
+            playAlbum('spotify:album:2LTqBgZUH4EkDcj8hdkNjK')
+        elif text == 'ALBUM CERTIFIED LOVER BOY':
+            # https://open.spotify.com/album/3SpBlxme9WbeQdI9kx7KAV
+            playAlbum('spotify:album:3SpBlxme9WbeQdI9kx7KAV')
+        elif text == 'TRACK NEVER GONNA GIVE YOU UP':
+            # https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT
+            playTrack('spotify:track:4cOdK2wGLETKBW3PvgPWqT')
+except serial.SerialException:
+    print('\033[91m' + 'ERROR: Check your serial port connection' + '\033[0m')
+except KeyboardInterrupt:
+    print('\033[92m' + "Exiting..." + '\033[0m')
+finally:
+    ser.close()
+    print('\033[92m' + "Closing serial connection..." + '\033[0m')
