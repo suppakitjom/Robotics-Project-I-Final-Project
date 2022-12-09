0d3de6dfb821705aeaa5436b66bd6b055591cae3
suppakitjom <99940355+suppakitjom@users.noreply.github.com>
2022-12-09 21:33:08 +0700

    finalllllll

--- src/main.py
+++ src/main.py
@@ -54,24 +54,31 @@ try:
         # play tracks called by ESP32
         if text == 'TRACK NEVER GONNA GIVE YOU UP':
             # https://open.spotify.com/track/4cOdK2wGLETKBW3PvgPWqT
+            print('Playing track: Never Gonna Give You Up (Rick Astley)')
             playTrack('spotify:track:4cOdK2wGLETKBW3PvgPWqT')
         elif text == 'PLAYLIST CHRISTMAS HITS':
             # https://open.spotify.com/playlist/37i9dQZF1DX0Yxoavh5qJV
+            print('Playing playlist: Christmas Hits')
             playPlaylist('spotify:playlist:37i9dQZF1DX0Yxoavh5qJV')
         elif text == 'ALBUM BLUE NEIGHBORHOOD':
             # https://open.spotify.com/album/2mRBvhDWqm8Fj2U0F6mMY4
+            print('Playing album: Blue Neighborhood by Troye Sivan')
             playAlbum('spotify:album:2mRBvhDWqm8Fj2U0F6mMY4')
         elif text == 'ALBUM BEINGFUNNYINFOREIGNLANGUAGE':
             # https://open.spotify.com/album/6dVCpQ7oGJD1oYs2fv1t5M
+            print('Playing album: BeingFunnyInForeignLanguage by The 1975')
             playAlbum('spotify:album:6dVCpQ7oGJD1oYs2fv1t5M')
         elif text == 'ALBUM SMITHEREENS':
             # https://open.spotify.com/album/2hEnymoejldpuxSdTnkard
+            print('Playing album: Smithereens by Joji')
             playAlbum('spotify:album:2hEnymoejldpuxSdTnkard')
         elif text == 'PLAYLIST JOMS ESSENTIALS':
             # https://open.spotify.com/playlist/2IbYQdnmVCr5l8UDR9Jw1l
+            print('Playing playlist: essentials')
             playPlaylist('spotify:playlist:2IbYQdnmVCr5l8UDR9Jw1l')
         elif text == 'ALBUM MAKING STEAK':
             # https://open.spotify.com/album/2KR8a0U0f286MuzLaEJhL6
+            print('Playing album: Making Steak by HYBS')
             playAlbum('spotify:album:2KR8a0U0f286MuzLaEJhL6')
 
 # error handlings
